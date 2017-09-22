b0b41ba391248da14a66df6bfdda1b763687d1fd
whoami <whoami@hellsystem>
2017-09-22 10:29:47 +0200

    Fix functions args

--- mem.c
+++ mem.c
@@ -12,15 +12,15 @@ void mem_init(char* mem, size_t taille){
 
 }
 
-void* mem_alloc(size_t){
+void* mem_alloc(size_t size){
 
 }
 
-void mem_free(void*){
+void mem_free(void *zone){
 
 }
 
-size_t mem_get_size(void *){
+size_t mem_get_size(void *zone){
 
 }
 
@@ -30,24 +30,15 @@ void mem_show(void (*print)(void *, size_t, int free)){
 
 }
 
-/* Choix de la strategie et strategies usuelles */
-typedef struct fb* (mem_fit_function_t)(struct fb *, size_t){
-
-}
-
-void mem_fit(mem_fit_function_t*){
-
-}
-
-struct fb* mem_fit_first(struct fb*, size_t){
+struct fb* mem_fit_first(struct fb *list, size_t size){
 
 }
 
 /* Si vous avez le temps */
-struct fb* mem_fit_best(struct fb*, size_t){
+struct fb* mem_fit_best(struct fb *list, size_t size){
 
 }
 
-struct fb* mem_fit_worst(struct fb*, size_t){
+struct fb* mem_fit_worst(struct fb *list, size_t size){
 
 }
