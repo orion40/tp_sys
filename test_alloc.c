46c112b2ce10f27525db68f28f8545956fb33a2e
whoami <whoami@hellsystem>
2017-10-01 08:12:26 +0200

    Add mem_free test

--- test_alloc.c
+++ test_alloc.c
@@ -4,11 +4,13 @@
 #include "mem.h"
 #include "common.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <time.h>
 #include <assert.h>
 
-#define NB_TESTS 10
+#define NB_TESTS 40
+#define NB_POINTERS 40
 
 static void *alloc(size_t estimate) {
     void *result;
@@ -19,7 +21,8 @@ static void *alloc(size_t estimate) {
 }
 
 static void *big_alloc(size_t estimate) {
-    void *result;
+    void* result;
+
     while ((result = mem_alloc(estimate)) == NULL) {
         estimate--;
     }
@@ -39,22 +42,22 @@ void alloc_big(){
 }
 
 void alloc_alot(){
+    void* pointers[NB_POINTERS];
     for (int i=0; i<NB_TESTS; i++) {
-        int size = rand()%1000;
-        alloc(size);
+        int size;
+        if (i < 10)
+            size = rand()%1000;
+        else if (i < 20)
+            size = rand()%2000;
+        else if (i < 30)
+            size = rand()%10000;
+        else if (i < 40)
+            size = rand()%40000;
+        pointers[i] = alloc(size);
     }
-    for (int i=0; i<NB_TESTS; i++) {
-        int size = rand()%2000;
-        alloc(size);
-    }
-    for (int i=0; i<NB_TESTS; i++) {
-        int size = rand()%10000;
-        alloc(size);
-    }
-    for (int i=0; i<NB_TESTS; i++) {
-        int size = rand()%40000;
-        alloc(size);
-    }
+    mem_show(afficher_zone);
+    free(pointers[10]);
+    alloc(rand()%1000);
 }
 
 int main(int argc, char *argv[]) {
@@ -64,8 +67,18 @@ int main(int argc, char *argv[]) {
             "\n");
     mem_init(get_memory_adr(), get_memory_size());
 
-    alloc_alot();
-    alloc_big();
+    char* test = (char*) alloc(50);
+    for (int i = 0; i < 50; i++){
+        test[i] = "K";
+    }
+    test[49] = 0;
+    printf("%s", test);
+    
+    mem_show(afficher_zone);
+    mem_free(test);
+    mem_show(afficher_zone);
+    //alloc_alot();
+    //alloc_big();
 
     // TEST OK
     return 0;
